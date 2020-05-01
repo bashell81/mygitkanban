fc3093f2a502e36f236cfc4da3b4270ecf14a629
bashell81 <liapple1122>
2020-05-01 09:18:18 +0800

    更新项目数据时只覆盖当天数据，保留历史数据用于趋势分析

--- dbstore.py
+++ dbstore.py
@@ -28,14 +28,14 @@ def insertProjectUpdateInfo(remoteurl,addline,delline,locline,users,curbranch,nd
 
     # 当前日期
     now = t.strftime('%Y-%m-%d %H:%M:%S')
-    #先查询看是否已存在该项目，没有就新增，有就更新
-    qry_cmd = "select * from %s where remoteurl='%s' and curbranch='%s'" % (tablename,remoteurl,curbranch)
+    #先查询看是否当天已存在该项目，没有就新增，有就更新
+    qry_cmd = "select * from %s where remoteurl='%s' and TO_DAYS(ts)=TO_DAYS(now()) and curbranch='%s'" % (tablename,remoteurl,curbranch)
     ret = cursor.execute(qry_cmd)
     if ret == 0:
         mysql_cmd = "INSERT INTO %s(remoteurl,addline,delline,locline,users,ts,curbranch)values('%s',%s,%s,%s,'%s','%s','%s')"%(tablename,remoteurl,addline,delline,locline,users,now,curbranch)
 
     else:
-        mysql_cmd = "UPDATE %s SET addline=%s,delline=%s,locline=%s,ts='%s',users ='%s' WHERE remoteurl='%s' and curbranch ='%s'" % (tablename,addline,delline,locline,now,users,remoteurl,curbranch)
+        mysql_cmd = "UPDATE %s SET addline=%s,delline=%s,locline=%s,ts='%s',users ='%s' WHERE remoteurl='%s' and TO_DAYS(ts)=TO_DAYS(now())  and curbranch ='%s'" % (tablename,addline,delline,locline,now,users,remoteurl,curbranch)
 
     print(mysql_cmd)
     cursor.execute(mysql_cmd)
